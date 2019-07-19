4c77e5a18da262b68a6d19abdb77a9ddbc4a8251
Arnold Fabriczy <arnold.fabriczy@studylink.com>
2019-07-19 16:11:39 +0930

    Message include private and public routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import { startLogout } from '../actions/auth';
 export const Header = ({startLogout}) => (
     <header>
         <h1>Expensify</h1>
-        <NavLink exact to="/" activeClassName="is-active">Go Home</NavLink>
+        <NavLink exact to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
         <NavLink exact to="/create" activeClassName="is-active">Create Expense</NavLink>
         <NavLink exact to="/help" activeClassName="is-active">Help</NavLink>
         <button onClick={startLogout}>Logout</button>
